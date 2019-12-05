ac4e5a69648774354a1741e85ebb16ee205808b6
lang <hoanglang@gmail.com>
2019-12-05 01:29:04 +0100

    Day 4 - Nicer Increasing pattern from Juju

--- src/day4/secure-container.ts
+++ src/day4/secure-container.ts
@@ -1,25 +1,27 @@
+const adjacentNumberPattern = /(\d)\1/;
+
 export function hasAdjacentNumbers(n: string): boolean {
-    return /(\d)\1/.test(n);
+    return adjacentNumberPattern.test(n);
 }
 
+const repeatingNumbersPattern = /(\d)\1{2,}/g;
+
 export function containsDouble(n: string): boolean {
-    const reduced = n.replace(/(\d)\1{2,}/g, 'x');
+    const reduced = n.replace(repeatingNumbersPattern, 'x');
     return hasAdjacentNumbers(reduced);
 }
 
+const increasingPattern = /^1*2*3*4*5*6*7*8*9*$/;
+
 export function onlyIncrease(n: string): boolean {
-    let res = true;
-    let prev = 1;
-    for (let i of n.split('').map(v => +v)) {
-        res = res && prev <= i;
-        prev = i;
-    }
-    return res;
+    return increasingPattern.test(n);
 }
 
 // Generator of range
 export function range(start, end): Array<number> {
-    return (new Array(end - start + 1)).fill(undefined).map((_, i) => i + start);
+    return (new Array(end - start + 1))
+        .fill(undefined)
+        .map((_, i) => i + start);
 }
 
 export function findAllPossiblePasswords(start: number, end: number): Array<string> {
